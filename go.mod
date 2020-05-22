1e4715f222051172f882615d30ee9da9a713b21f
Jan Hajek <hajek.mail@gmail.com>
2020-05-22 10:19:00 +0200

    aaa

--- go.mod
+++ go.mod
@@ -1,1 +1,5 @@
-module "githubtest"
+module githubtest
+
+go 1.14
+
+require github.com/gorilla/mux v1.7.4 // indirect
--- main.go
+++ main.go
@@ -1,7 +1,51 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+	"os"
+)
 
 func main() {
-	fmt.Println("main")
+	fmt.Println("run")
+
+	run()
+}
+
+func run() {
+	r := mux.NewRouter()
+
+
+	// rest
+	r.HandleFunc("/test", serve)
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	log.Fatal(http.ListenAndServe(":"+port, r))
+}
+
+func serve(w http.ResponseWriter, r *http.Request) {
+
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/json")
+
+
+	result := struct {
+		Kolo string
+	} {
+		Kolo: "asd",
+	}
+
+	data, err := json.Marshal(&result)
+	if err != nil {
+		panic(err)
+	}
+
+	w.Write(data)
 }
